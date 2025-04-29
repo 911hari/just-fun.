a3d265d9024180c91055e63b08cadb271680bc53
911hari <harichandsabu@gmail.com>
2025-04-29 14:16:46 +0530

    Add files via upload

---
+++ function showMessage() {.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    const surpriseMessage = document.getElementById('surpriseMessage');
+    surpriseMessage.style.display = 'block';
+    surpriseMessage.classList.add('fadeIn');
+}
